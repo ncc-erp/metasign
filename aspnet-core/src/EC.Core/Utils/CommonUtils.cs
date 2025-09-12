b53ec27ffe70cc01c3a79108bcaeae4f63a6e4f0
truong-nguyenmanh <truong.nguyenmanh@ncc.asia>
2025-09-12 15:40:02 +0700

    Fixe: email template A tag styling

--- aspnet-core/src/EC.Core/Utils/CommonUtils.cs
+++ aspnet-core/src/EC.Core/Utils/CommonUtils.cs
@@ -129,7 +129,7 @@ namespace EC.Utils
                 .Replace("{{SignUrl}}", content.SignUrl)
                 .Replace("{{AuthorEmail}}", $" {content.AuthorName} ({content.AuthorEmail}) ")
                 .Replace("{{ContractCode}}</h3>", $"{content.ContractCode}</h3>")
-                .Replace("{{LookUpContactUrl}}", content.LookupUrl)
+                .Replace("{{LookUpContractUrl}}", content.LookupUrl)
                 .Replace("{{ExpireTimeTag}}", $"{expireTimeTag}<h3>ID: {content.ContractGuid}</h3>");
 
             return newString;
