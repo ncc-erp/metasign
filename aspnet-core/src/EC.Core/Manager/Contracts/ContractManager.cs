46069733069dc3b6ce4400dad9ceedd218097081
truong-nguyenmanh <truong.nguyenmanh@ncc.asia>
2025-10-24 13:12:03 +0700

    fix: clean up duplicate content after merge resolution

--- aspnet-core/src/EC.Core/Manager/Contracts/ContractManager.cs
+++ aspnet-core/src/EC.Core/Manager/Contracts/ContractManager.cs
@@ -424,7 +424,6 @@ namespace EC.Manager.Contracts
                 {
                     ContractId = entity.Id,
                     MailContent = GetContractMailContent(entity.Id, input.MailTemplateId)
-                    MailContent = GetContractMailContent(entity.Id, input.MailTemplateId)
                 };
                 await SendMailToViewer(sendMailDto);
                 await SendMail(sendMailDto);
